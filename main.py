6f5dea8eec85347a01ac412cc1183465812431e4
Bitna Lee <bitnaleeeee@gmail.com>
2023-01-21 16:33:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ MAX_POST = 5
 
 markdown_text = """
 ## Enjoys solving problems and controlling flow with JavaScript
-
+[![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fbitnaleeeee&count_bg=%23555555&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)
 ## Recent Post 
 
 """  # list of blog posts will be appended here
