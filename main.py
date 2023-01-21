7e0a152eeba6906819e43191e975d6ccb5a19ac2
Bitna Lee <bitnaleeeee@gmail.com>
2023-01-21 16:57:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ RSS_FEED = feedparser.parse(URL)
 MAX_POST = 5
 
 markdown_text = """
-## Enjoys solving problems and controlling flow with JavaScript
+### Enjoys solving problems and controlling flow with JavaScript
 [![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fbitnaleeeee&count_bg=%23555555&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)
 ## Recent Post 
 
